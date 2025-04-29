4afc9a85e0467bec94289d0659b38fa64dc86861
Borislav <bvasilev338@gmail.com>
2025-04-29 14:11:28 +0300

    adding functionality for All Posts

--- Forum/Controllers/PostController.cs
+++ Forum/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Forum.Core.Contracts;
+using Forum.Core.Models.Post;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,12 @@ namespace Forum.Controllers
     public class PostController : BaseController
     {
         private readonly IPostService postService;
+        private readonly IThreadService threadService;
 
-        public PostController(IPostService _postService)
+        public PostController(IPostService _postService, IThreadService _threadService)
         {
             postService = _postService;
+            threadService = _threadService;
         }
         [AllowAnonymous]
         public async Task<IActionResult> All()
@@ -23,16 +26,25 @@ namespace Forum.Controllers
             }
 
             return View(model);
-        }   
-
-        public IActionResult Index()
-        {
-            return View();
         }
 
+
+        [HttpGet]
         public async Task<IActionResult> Add()
         {
-            return View();
+            var threadName = await threadService.GetAllThreadsNameAsync();
+            var model = new AddPostFormViewModel
+            {
+                Threads = threadName
+            };
+
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            return View(model);
+        
         }
 
         public async Task<IActionResult> Mine()
--- Forum/Core/Services/ThreadService.cs
+++ Forum/Core/Services/ThreadService.cs
@@ -1,8 +1,62 @@
 ﻿using Forum.Core.Contracts;
+using Forum.Core.Models.Category;
+using Forum.Core.Models.Thread;
+using Forum.Infrastructure.Common;
+using Forum.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+using System.Globalization;
 
 namespace Forum.Core.Services
 {
     public class ThreadService : IThreadService
     {
+
+        private readonly IRepository repository;
+
+        public ThreadService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+
+        public async Task<IEnumerable<AllThreadsViewModel>> AllThreadAsync()
+        {
+            var threads = await repository.All<Infrastructure.Data.Models.Thread>()
+               
+                .Select(t => new AllThreadsViewModel
+                {
+                    Name = t.ThreadName,
+                    CreatedDate = t.CreatedAt.ToString("dd MMMM yyyy", CultureInfo.InvariantCulture),
+                    Category = t.Category.Name 
+                })
+                .ToListAsync();
+
+            return threads;
+        }
+
+        public async Task AddThreadAsync(AddThreadViewModel model, string userId)
+        {
+            var thread = new Infrastructure.Data.Models.Thread
+            {
+                ThreadName = model.Name,
+                CategoryId = model.CategoryId, 
+                UserId = userId,  
+                CreatedAt = DateTime.UtcNow 
+            };
+
+            await repository.AddAsync(thread);
+            await repository.SaveChangesAsync(); 
+        }
+
+        public async Task<IEnumerable<ThreadViewModel>> GetAllThreadsNameAsync()
+        {
+            return await repository.All<Infrastructure.Data.Models.Thread>()
+               .Select(t => new ThreadViewModel
+               {
+                   Id = t.Id,
+                   Name = t.ThreadName,
+               }).ToListAsync();
+        }
     }
 }
