1d5fa7472cf4ed91bcb8d355bcb425150a69cf0e
Borislav <bvasilev338@gmail.com>
2025-10-25 11:41:55 +0300

    added null condition

--- Forum/Core/Models/Comment/CommentFormModel.cs
+++ Forum/Core/Models/Comment/CommentFormModel.cs
@@ -4,7 +4,7 @@ public class CommentFormModel
 {
     [Required]
     [StringLength(500, MinimumLength = 1)]
-    public string Content { get; set; }
+    public string? Content { get; set; }
 
     public int PostId { get; set; }
 }
